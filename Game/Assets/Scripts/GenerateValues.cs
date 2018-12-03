927e4f4d7878f24b3665e3c9b5ce451078db99ca
Elisa Mamos <temerair@op.pl>
2018-12-03 17:11:31 +0100

    More sprites for learn numbers game

--- Game/Assets/Scripts/GenerateValues.cs
+++ Game/Assets/Scripts/GenerateValues.cs
@@ -4,70 +4,37 @@ using UnityEngine;
 
 public class GenerateValues : MonoBehaviour
 {
-
-    public Sprite sprite1;
-    public Sprite sprite2;
-    public Sprite sprite3;
-    public Sprite sprite4;
-    public Sprite sprite5;
-    public Sprite sprite6;
-    public Sprite sprite7;
-    public Sprite sprite8;
-    public Sprite sprite9;
-    public Sprite sprite10;
-    public Sprite sprite11;
-    public Sprite sprite12;
-    public Sprite sprite13;
-    public Sprite sprite14;
-
-    int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+    public List<Sprite> spriteList = new List<Sprite>();
+    int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33 };
     List<int> list = new List<int>();
     List<int> listButton = new List<int>();
     List<int> listNumber = new List<int>();
     List<int> listObject = new List<int>();
     List<int> listHelp = new List<int>();
-
+    
     Dictionary<string, Sprite> spriteDictionary = new Dictionary<string, Sprite>();
     Dictionary<int, string> dictionary = new Dictionary<int, string>();
 
     public void Startt()
     {
+        for (int i = 1; i < 33; i++)
+        {
+         
+              string name = "img" + i;
+           
+            string spriteName = "sprite" + i;
+            dictionary.Add(i, name);
+           
+            spriteDictionary.Add(spriteName, spriteList[i-1]);
+        }
 
-        dictionary.Add(1, "img1");
-        dictionary.Add(2, "img2");
-        dictionary.Add(3, "img3");
-        dictionary.Add(4, "img4");
-        dictionary.Add(5, "img5");
-        dictionary.Add(6, "img6");
-        dictionary.Add(7, "img7");
-        dictionary.Add(8, "img8");
-        dictionary.Add(9, "img9");
-        dictionary.Add(10, "img10");
-        dictionary.Add(11, "img11");
-        dictionary.Add(12, "img12");
-        dictionary.Add(13, "img13");
-        dictionary.Add(14, "img14");
-
-
-        spriteDictionary.Add("sprite1", sprite1);
-        spriteDictionary.Add("sprite2", sprite2);
-        spriteDictionary.Add("sprite3", sprite3);
-        spriteDictionary.Add("sprite4", sprite4);
-        spriteDictionary.Add("sprite5", sprite5);
-        spriteDictionary.Add("sprite6", sprite6);
-        spriteDictionary.Add("sprite7", sprite7);
-        spriteDictionary.Add("sprite8", sprite8);
-        spriteDictionary.Add("sprite9", sprite9);
-        spriteDictionary.Add("sprite10", sprite10);
-        spriteDictionary.Add("sprite11", sprite11);
-        spriteDictionary.Add("sprite12", sprite12);
-        spriteDictionary.Add("sprite13", sprite13);
-        spriteDictionary.Add("sprite14", sprite14);
+     
 
         list.AddRange(numbers);
 
-        for (int i = 0; i < 14; i++)
+        for (int i = 0; i < 33; i++)
         {
+            Debug.Log(i);
             listNumber.Add(list[i]);
         }
     }
@@ -91,7 +58,8 @@ public class GenerateValues : MonoBehaviour
     public void AddImagesToObjectsList(int objectCount)
     {
         listObject.Clear();
-        var randomObject = Random.Range(10, 14);
+        var randomObject = Random.Range(10, spriteList.Count);
+       // Debug.Log(randomObject);
         for (int i = 0; i < objectCount; i++)
         {
             listObject.Add(list[randomObject]);
