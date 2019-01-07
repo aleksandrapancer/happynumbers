6b82327f5d1835ce0ae175e210f5e0d476a518d5
Elisa Mamos <temerair@op.pl>
2019-01-07 12:31:52 +0100

    Finally working <fixed back button problem>

--- Game/Assets/Scripts/MainMenu/MenuBack.cs
+++ Game/Assets/Scripts/MainMenu/MenuBack.cs
@@ -1,9 +1,17 @@
 ﻿using Assets.Scripts.MainMenu;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MenuBack : MonoBehaviour
+public class MenuBack : MonoBehaviour, IPointerDownHandler
 {
-    private void OnMouseDown()
+    public void OnMouseDown()
+    {
+        GameManager.Instance?.OnBackButtonClick();
+        Debug.Log("Back!");
+
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
     {
         GameManager.Instance?.OnBackButtonClick();
         Debug.Log("Back!");
